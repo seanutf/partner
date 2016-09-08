8bb8bd1e1b259d65046b9ccac28d242dbb062b8e
yi.zhang <seanutf@gmail.com>
2016-09-08 17:12:25 +0800

    加入bmob移动后端

---
+++ app/src/main/java/com/sean/partner/meta/User.java
@@ -0,0 +1,17 @@
+package com.sean.partner.meta;
+
+import cn.bmob.v3.BmobUser;
+
+/**
+ * Created by sks on 2016/9/8.
+ */
+public class User extends BmobUser{
+    /*  BmobUser除了从BmobObject继承的属性外，还有几个特定的属性：
+        username: 用户的用户名（必需）。
+        password: 用户的密码（必需）。
+        email: 用户的电子邮件地址（可选）。
+        emailVerified:邮箱认证状态（可选）。
+        mobilePhoneNumber：手机号码（可选）。
+        mobilePhoneNumberVerified：手机号码的认证状态（可选）。
+    * */
+}
--- app/src/main/java/com/sean/partner/view/activity/SplashActivity.java
+++ app/src/main/java/com/sean/partner/view/activity/SplashActivity.java
@@ -1,9 +1,11 @@
-package com.sean.partner;
+package com.sean.partner.view.activity;
 
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
+
+import com.sean.partner.R;
 
 import cn.bmob.v3.Bmob;
 
@@ -24,7 +26,7 @@ public class SplashActivity extends Activity {
         //提供以下两种方式进行初始化操作：
 
         //第一：默认初始化
-        Bmob.initialize(this, "Your Application ID");
+        Bmob.initialize(this, getString(R.string.bmob_application_id));
 
         //第二：自v3.4.7版本开始,设置BmobConfig,允许设置请求超时时间、文件分片上传时每片的大小、文件的过期时间(单位为秒)，
         //BmobConfig config =new BmobConfig.Builder(this)
