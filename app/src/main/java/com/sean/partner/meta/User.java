b75a731d62ae684e07fef62c8cc9a197fb630d25
yi.zhang <seanutf@gmail.com>
2016-09-09 15:00:56 +0800

    填充user信息

--- app/src/main/java/com/sean/partner/meta/User.java
+++ app/src/main/java/com/sean/partner/meta/User.java
@@ -14,4 +14,85 @@ public class User extends BmobUser{
         mobilePhoneNumber：手机号码（可选）。
         mobilePhoneNumberVerified：手机号码的认证状态（可选）。
     * */
+
+    private Boolean sex;        //性别
+    private String avatar;      //头像
+    private Integer age;        //年龄
+    private Long createTime;    //加入时间
+    private Integer level;      //等级
+    private Integer score;      //积分
+    private String desc;        //签名
+    private String addr;        //地址
+    //未确定数据类型和待扩充的信息
+//    private String realName;    //真实姓名
+//    private String idCard;      //身份证号
+//    private String following;   //关注的人
+//    private String followed;    //被谁关注
+//    private String group;       //群
+//    private String interest;    //兴趣
+
+
+    public Boolean getSex() {
+        return sex;
+    }
+
+    public void setSex(Boolean sex) {
+        this.sex = sex;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getLevel() {
+        return level;
+    }
+
+    public void setLevel(Integer level) {
+        this.level = level;
+    }
+
+    public Long getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Long createTime) {
+        this.createTime = createTime;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
 }
