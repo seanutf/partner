1aa9df05333c497278ebdbd5ffc6dd16db8f4b47
yi.zhang <seanutf@gmail.com>
2016-09-12 19:04:22 +0800

    新增用户引导页面

--- app/src/main/java/com/sean/partner/view/activity/UserGuideActivity.java
+++ app/src/main/java/com/sean/partner/view/activity/UserGuideActivity.java
@@ -3,6 +3,9 @@ package com.sean.partner.view.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+
+import com.sean.partner.R;
 
 /**
  * Created by sean on 2016/9/12.
@@ -12,5 +15,7 @@ public class UserGuideActivity extends AppCompatActivity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        setContentView(R.layout.activity_user_guide);
     }
 }
