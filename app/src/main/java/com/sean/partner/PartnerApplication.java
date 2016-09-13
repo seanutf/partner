65e65a9cb7ca6e5b597dde0050caa00c079e9d25
yi.zhang <seanutf@gmail.com>
2016-09-13 17:55:41 +0800

    一期的引导页面完成

--- app/src/main/java/com/sean/partner/PartnerApplication.java
+++ app/src/main/java/com/sean/partner/PartnerApplication.java
@@ -3,8 +3,12 @@ package com.sean.partner;
 import android.app.Application;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.widget.Toast;
 
 import com.sean.partner.Utils.UserConfigures;
+import com.sean.partner.meta.User;
+
+import cn.bmob.v3.BmobUser;
 
 /**
  * Created by sean on 2016/9/9.
@@ -31,5 +35,34 @@ public class PartnerApplication  extends Application{
            e.printStackTrace();
            return "";
         }
+    }
+
+    /**
+     * 获取当前本地用户
+     */
+    public boolean getCurrentUser() {
+        User myUser = BmobUser.getCurrentUser(User.class);
+        if (myUser != null) {
+//			log("本地用户信息:objectId = " + myUser.getObjectId() + ",name = " + myUser.getUsername()
+//					+ ",age = "+ myUser.getAge());
+            Toast.makeText(this,"本地用户信息:objectId = " + myUser.getObjectId() + ",name = " + myUser.getUsername()
+                    + ",age = "+ myUser.getAge(),Toast.LENGTH_LONG).show();
+            return true;
+        } else {
+            //toast("本地用户为null,请登录。");
+            Toast.makeText(this,"本地用户为null,请登录。",Toast.LENGTH_SHORT).show();
+        }
+        //V3.4.5版本新增加getObjectByKey方法获取本地用户对象中某一列的值
+//        String username = (String) BmobUser.getObjectByKey("username");
+//        Integer age = (Integer) BmobUser.getObjectByKey("age");
+//        Boolean sex = (Boolean) BmobUser.getObjectByKey("sex");
+//        JSONArray hobby= (JSONArray) BmobUser.getObjectByKey("hobby");
+//        JSONArray cards= (JSONArray) BmobUser.getObjectByKey("cards");
+//        JSONObject banker= (JSONObject) BmobUser.getObjectByKey("banker");
+//        JSONObject mainCard= (JSONObject) BmobUser.getObjectByKey("mainCard");
+//        log("username："+username+",\nage："+age+",\nsex："+ sex);
+//        log("hobby:"+(hobby!=null?hobby.toString():"为null")+"\ncards:"+(cards!=null ?cards.toString():"为null"));
+//        log("banker:"+(banker!=null?banker.toString():"为null")+"\nmainCard:"+(mainCard!=null ?mainCard.toString():"为null"));
+        return false;
     }
 }
--- app/src/main/java/com/sean/partner/view/activity/SplashActivity.java
+++ app/src/main/java/com/sean/partner/view/activity/SplashActivity.java
@@ -32,20 +32,16 @@ public class SplashActivity extends Activity {
         //发现有展示过引导页面，直接去登录等等，没有展示过，去展示
         if(getGuide()){
             Toast.makeText(this,"不展示引导页面",Toast.LENGTH_SHORT).show();
-            if(getCurrentUser()){
+            if(((PartnerApplication)getApplication()).getCurrentUser()){
                 //todo 直接展示主界面
             } else {
                 //todo 展示登录注册页面
             }
         } else {
-            //todo 引导页面,并且在引导页面检查用户登录情况
+            //引导页面
             Toast.makeText(this,"展示引导页面",Toast.LENGTH_SHORT).show();
             startActivity(new Intent(this,UserGuideActivity.class));
             finish();
-
-//            if(getCurrentUser()){
-//
-//            }
         }
     }
 
@@ -75,34 +71,5 @@ public class SplashActivity extends Activity {
         //.setFileExpiration(2500)
         //.build();
         //Bmob.initialize(config);
-    }
-
-    /**
-     * 获取当前本地用户
-     */
-    private boolean getCurrentUser() {
-		User myUser = BmobUser.getCurrentUser(User.class);
-		if (myUser != null) {
-//			log("本地用户信息:objectId = " + myUser.getObjectId() + ",name = " + myUser.getUsername()
-//					+ ",age = "+ myUser.getAge());
-            Toast.makeText(this,"本地用户信息:objectId = " + myUser.getObjectId() + ",name = " + myUser.getUsername()
-                    + ",age = "+ myUser.getAge(),Toast.LENGTH_LONG).show();
-            return true;
-		} else {
-			//toast("本地用户为null,请登录。");
-            Toast.makeText(this,"本地用户为null,请登录。",Toast.LENGTH_SHORT).show();
-		}
-        //V3.4.5版本新增加getObjectByKey方法获取本地用户对象中某一列的值
-//        String username = (String) BmobUser.getObjectByKey("username");
-//        Integer age = (Integer) BmobUser.getObjectByKey("age");
-//        Boolean sex = (Boolean) BmobUser.getObjectByKey("sex");
-//        JSONArray hobby= (JSONArray) BmobUser.getObjectByKey("hobby");
-//        JSONArray cards= (JSONArray) BmobUser.getObjectByKey("cards");
-//        JSONObject banker= (JSONObject) BmobUser.getObjectByKey("banker");
-//        JSONObject mainCard= (JSONObject) BmobUser.getObjectByKey("mainCard");
-//        log("username："+username+",\nage："+age+",\nsex："+ sex);
-//        log("hobby:"+(hobby!=null?hobby.toString():"为null")+"\ncards:"+(cards!=null ?cards.toString():"为null"));
-//        log("banker:"+(banker!=null?banker.toString():"为null")+"\nmainCard:"+(mainCard!=null ?mainCard.toString():"为null"));
-        return false;
     }
 }
