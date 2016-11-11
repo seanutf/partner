1624102079f5be1105127c8ce4cb94aa7d124ebb
yi.zhang <seanutf@gmail.com>
2016-11-11 14:24:45 +0800

    增加

--- app/src/main/java/com/sean/partner/view/activity/MainActivity.java
+++ app/src/main/java/com/sean/partner/view/activity/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.sean.partner.R;
+
 /**
  * Created by sean on 2016/9/14.
  */
@@ -12,5 +14,6 @@ public class MainActivity extends AppCompatActivity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
 }
