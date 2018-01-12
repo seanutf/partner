33130047d22b2298f0c9c7f0bab79ef586a7fadd
sean <seanutf@gmail.com>
2018-01-12 22:19:05 +0800

    1.项目添加kotlin支持2，梳理dimens文件3，规范引用

---
+++ app/src/main/java/com/sean/partner/demo/KotlinDemo.kt
@@ -0,0 +1,23 @@
+package com.sean.partner.demo
+
+
+/**
+ * Created by Sean on 2018/1/11.
+ *
+ */
+
+   fun max(a: Int, b: Int):Int{
+    return if(a >b)a else b
+    }
+ data class KotlinDemo(val name: String, val age:Int? = null)
+
+    fun main(args: Array<String>){
+        println("Hello world!")
+    }
+
+    fun max(a:Int, b: Int, c:Int):Int = if(a > b) a else b;
+
+fun getMax(a:Int, i:String):String =  ""
+
+  val question = "ffff"
+var aa:Int  = 4
