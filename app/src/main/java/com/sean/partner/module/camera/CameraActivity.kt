fae59d7706c284296293df5bde7ce3c1f6bbc331
Windows-PC\Administrator <seanutf@gmail.com>
2018-03-20 18:28:00 +0800

    添加拍照模块和拍照界面

--- app/src/main/java/com/sean/partner/module/camera/CameraActivity.kt
+++ app/src/main/java/com/sean/partner/module/camera/CameraActivity.kt
@@ -9,5 +9,12 @@ class CameraActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_layout_camera)
+        if (null == savedInstanceState) {
+            fragmentManager.beginTransaction()
+                    .replace(R.id.container, CameraFragment.newInstance())
+                    .commit()
+        }
     }
+
+
 }
