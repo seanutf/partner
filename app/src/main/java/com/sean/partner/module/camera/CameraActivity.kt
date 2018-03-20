4a0c92bc176ccaf867ce78ae11e1573081c7bd67
Windows-PC\Administrator <seanutf@gmail.com>
2018-03-20 17:30:20 +0800

    添加拍照模块和拍照界面

---
+++ app/src/main/java/com/sean/partner/module/camera/CameraActivity.kt
@@ -0,0 +1,13 @@
+package com.sean.partner.module.camera
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.sean.partner.R
+
+class CameraActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_layout_camera)
+    }
+}
