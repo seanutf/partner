b436877a57714658a6588f77c74ca0dbec9edb6e
sean <seanutf@gmail.com>
2018-04-06 15:44:02 +0800

    加入拍照模块，且支持前置摄像头不能自动对焦模式

--- app/src/main/java/com/sean/partner/module/camera/CameraActivity.kt
+++ app/src/main/java/com/sean/partner/module/camera/CameraActivity.kt
@@ -1,5 +1,7 @@
 package com.sean.partner.module.camera
 
+import android.os.Build
+import android.os.Build.VERSION_CODES.LOLLIPOP
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.sean.partner.R
@@ -9,12 +11,16 @@ class CameraActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_layout_camera)
-        if (null == savedInstanceState) {
-            fragmentManager.beginTransaction()
-                    .replace(R.id.container, CameraFragment.newInstance())
-                    .commit()
+        if (null == savedInstanceState){
+            if(Build.VERSION.SDK_INT >= LOLLIPOP)
+                supportFragmentManager.beginTransaction()
+                        .replace(R.id.container, Camera21Fragment.newInstance())
+                        .commit()
+            else
+                supportFragmentManager.beginTransaction()
+                        .replace(R.id.container, Camera21Fragment.newInstance())
+                        .commit()
         }
     }
-
 
 }
