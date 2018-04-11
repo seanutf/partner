0cbebc71465e175029619ea5c7a83f2f5e428659
Windows-PC\Administrator <seanutf@gmail.com>
2018-04-11 11:46:05 +0800

    添加设置页面

--- app/src/main/java/com/sean/partner/module/main/HomeActivity.java
+++ app/src/main/java/com/sean/partner/module/main/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.sean.partner.module.main;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -12,6 +13,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -113,6 +115,7 @@ public class HomeActivity extends MainActivity
         bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         bottomNavigationView.setSelectedItemId(R.id.navigation_home);
         mPresenter = new HomeModelPresenter(homeFragment);
+        initImmersive();
     }
 
     @Override
@@ -151,7 +154,19 @@ public class HomeActivity extends MainActivity
         }
     }
 
-
+    // todo android4.4的通知栏问题
+    private void initImmersive() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            WindowManager.LayoutParams localLayoutParams = getWindow().getAttributes();
+            localLayoutParams.flags = (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | localLayoutParams.flags);
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+                //将侧边栏顶部延伸至status bar
+                drawer.setFitsSystemWindows(true);
+                //将主页面顶部延伸至status bar;虽默认为false,但经测试,DrawerLayout需显示设置
+                drawer.setClipToPadding(false);
+            }
+        }
+    }
 
 
 
