47fafc8ad54e9a9c2988200460432a625ce95a71
Windows-PC\Administrator <seanutf@gmail.com>
2018-04-16 11:36:46 +0800

    添加个人设置页面

---
+++ app/src/main/java/com/sean/partner/module/setting/profile/presenter/IProfileSettingPresenter.java
@@ -0,0 +1,10 @@
+package com.sean.partner.module.setting.profile.presenter;
+
+import com.sean.partner.global.interfaces.BasePresenter;
+
+/**
+ * Created by Sean on 2018/4/16.
+ */
+
+public interface IProfileSettingPresenter extends BasePresenter {
+}
---
+++ app/src/main/java/com/sean/partner/module/setting/profile/view/fragment/ProfileSettingFragment.java
@@ -0,0 +1,36 @@
+package com.sean.partner.module.setting.profile.view.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sean.partner.MainFragment;
+
+/**
+ * Created by Sean on 2018/4/16.
+ * 个人资料的设置页面
+ */
+
+public class ProfileSettingFragment extends MainFragment {
+
+    @Override
+    protected void initViewData(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected View createView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return null;
+    }
+
+    @Override
+    protected void setViewListener() {
+
+    }
+
+    @Override
+    protected void initView(View view) {
+
+    }
+}
