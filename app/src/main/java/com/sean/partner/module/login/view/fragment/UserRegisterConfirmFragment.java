9b6d8bcf2858dabc1ae68041f7d62d835bd10117
Windows-PC\Administrator <seanutf@gmail.com>
2018-04-23 17:08:50 +0800

    注册登录

--- app/src/main/java/com/sean/partner/module/login/view/fragment/UserRegisterConfirmFragment.java
+++ app/src/main/java/com/sean/partner/module/login/view/fragment/UserRegisterConfirmFragment.java
@@ -1,11 +1,7 @@
 package com.sean.partner.module.login.view.fragment;
 
-import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
